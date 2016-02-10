936bd2fc1b285bc0d7d32599d7a8e53ef1ee43f1
Sam Beaumont <SamABeaumont@users.noreply.github.com>
2016-02-10 09:09:39 -0800

    Update Dictionary.java

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -6,15 +6,25 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public final class Dictionary {
-	private static ArrayList<CanonicalWord> canonicalWords;
+	public static final boolean DEBUG = true;
 	
-	static {
+	private static boolean created = false;
+	
+	private ArrayList<CanonicalWord> canonicalWords;
+	
+	public Dictionary() {
+		if (created) {
+			throw new IllegalStateException("Only one instance of the Dictionary class may exist"
+					+ " at any given time.");
+		}
+		log("Began executing constructor.");
+		long startTime = System.currentTimeMillis();
 		Scanner dict = null;
 		boolean isCanonical = false;
 		if (new File("canon.txt").exists()) {
 			try {
 				dict = new Scanner(new File("canon.txt"));
-			} catch (FileNotFoundException f) {}
+			} catch (FileNotFoundException f) {} // already called exists(), will not execute
 			isCanonical = true;
 		} else {
 			try {
@@ -26,28 +36,51 @@ public final class Dictionary {
 		}
 		
 		if (isCanonical) {
-			dict.useDelimiter("[(\\r\\n)\n]{2}");
+			log("Executing canonical block.");
+			dict.useDelimiter("[(\\r\\n)\\n]{2}");
 			while (dict.hasNext()) {
 				String canon = dict.next();
 				Scanner sc = new Scanner(canon);
-				CanonicalWord w;
+				CanonicalWord w = new CanonicalWord(sc.nextLine());
 				while (sc.hasNextLine()) {
-					w = new CanonicalWord(sc.nextLine());
+					w.add(sc.nextLine());
 				}
 				sc.close();
+				canonicalWords.add(w);
 			}
 		} else {
+			log("Executing non-canonical block.");
 			while (dict.hasNextLine()) {
 				String input = dict.nextLine();
+				log("Reading input: " + input);
 				int index = Collections.binarySearch(canonicalWords,
 						new CanonicalWord(input));
+				if (index > 0) {
+					canonicalWords.get(index).add(input);
+				} else {
+					canonicalWords.add(-(index + 1), new CanonicalWord(input));
+				}
 			}
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println(((double) (endTime - startTime)) / 1000);
+		created = true;
+	}
+	
+	public void printAnagrams (String word) {
+		int index = Collections.binarySearch(canonicalWords, new CanonicalWord(word));
+		if (index < 0) {
+			
 		}
 	}
 	
-	public void print(PrintStream out) {
+	public void save(PrintStream out) {
 		for (CanonicalWord w : canonicalWords) {
-			w.print(out);
+			w.print(out, true);
 		}
+	}
+	
+	public static void log(String text) {
+		if (DEBUG) System.out.println(text);
 	}
 }
