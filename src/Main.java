28e1242695ef419e8a26b0a9b62806ba1d325007
Sam Beaumont <SamABeaumont@users.noreply.github.com>
2016-02-10 09:13:25 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
-public final class Main {
+import javax.swing.JOptionPane;
+
+public final class Main {	
 	public static void main (String[] args) throws InterruptedException {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to the anagram finder.");
 		System.out.print("Loading dictionary...");
+		Dictionary dict = new Dictionary();
 		while (input.hasNext()) {
 			if (input.equals("-quit")) {
-				System.out.println("Saving dictionary files...");
+				input.close();
+				if (!new File("canon.txt").exists()) {
+					System.out.println("Saving dictionary file...");
+					try {
+						dict.save(new PrintStream(new File("canon.txt")));
+					} catch (FileNotFoundException e) {
+						JOptionPane.showMessageDialog(null, "Unable to load dictionary file.", "",
+								JOptionPane.ERROR_MESSAGE);
+						e.printStackTrace();
+						System.exit(1);
+					}
+				}
 				System.exit(0);
 			} else {
 				
